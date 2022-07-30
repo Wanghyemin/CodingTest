0ff1133d0576d8a127d91e14ca3e835919bdd874
Mini's note <95896024+Wanghyemin@users.noreply.github.com>
2022-07-30 21:12:47 +0900

    Update p12977.java
    12977 finish

--- src/coding/programmers/p12977.java
+++ src/coding/programmers/p12977.java
@@ -1,36 +1,39 @@
 package coding.programmers;
 
 public class p12977 {
-
+	
+	
+	// 프로그래머스 : 소수 만들기 (12977)
 	
 	class Solution {
-	    public int solution(int[] nums) {
-	        int answer = 0;
-	        int sum = 0;
+    
+    public int isPrime(int n){
 
-	        for(int i =0; i<nums.length-2 ; i++){
-	            for(int j =i+1; j<nums.length-1 ; j++){
-	                for(int k =j+1; k<nums.length ; k++){
-	                    
-	                    sum = nums[i]+nums[j]+nums[k];
-	                         // 여기서부터 수정
-	                    	// 테스트
-	                        
-	                    for (int p = 2; p<=(int)Math.sqrt(sum); p++) {
-	                          if (sum % p == 0) { answer++; System.out.print(answer); }
-	                        
-	                    }
-	                }
-	            }
-	        }
-	        return answer;
-	    }
-	}
-	
-	
-	public static void main(String[] args) {
+        for (int m = 2; m<=(int)Math.sqrt(n); m++) {
+            if (n % m == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }          
+    
+    
+    public int solution(int[] nums) {
+        int answer = 0;
+        int sum = 0;
 
-
-	}
+        for(int i =0; i<nums.length-2 ; i++){
+            for(int j =i+1; j<nums.length-1 ; j++){
+                for(int k =j+1; k<nums.length ; k++){
+                    
+                    sum = nums[i]+nums[j]+nums[k];       
+                    answer += this.isPrime(sum);   
+             
+                    }
+                }
+            }
+        return answer;
+    }
+}
 
 }
