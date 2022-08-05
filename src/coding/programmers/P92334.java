2e3d1b7b4aeaf1f81a094464d6bbae6e2cedc839
왕혜민 <95896024+Wanghyemin@users.noreply.github.com>
2022-08-05 09:15:28 +0900

    Update P92334.java
    solve : String use ".equals()" method
    problem : "B_new" is null in java:36

--- src/coding/programmers/P92334.java
+++ src/coding/programmers/P92334.java
@@ -1,4 +1,8 @@
+package coding.programmers;
+
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 
 public class p92334 {
 
@@ -6,7 +10,7 @@ public class p92334 {
 
 
 		String[] report = {"1 3", "2 4"};
-		String [] id_list = {"1","2"};
+		String [] id_list = {"1","2","3","4"};
 		int k = 1;
 		////////////////////////////////////// 
 		
@@ -16,6 +20,7 @@ public class p92334 {
         HashSet<String> B_new = new HashSet<String>();
         int[] BB = new int[id_list.length]; // 중복되지 않는 신고당한 횟수
         int[] AB = new int[id_list.length];
+        Iterator<String> iter;
         
         for(int i = 0; i<report.length; i++){
         	report_sep = report[i].split(" ");
@@ -24,47 +29,39 @@ public class p92334 {
             
             report_sep=null;
         }
-        
-        System.out.println(B[1] + B[0]);
-        
-        
-        for(int i = 0; i<report.length; i++){
+
+        for(int i = 0; i<id_list.length; i++){
         	for (int j=0; j<A.length; j++) {
-            	if( id_list[i] == A[j]) { // i유저 == j신고자 : 신고자
+            	if( id_list[i].equals(A[j])) { // i유저 == j신고자 : 신고자
             		B_new.add(B[j]); // i유저에 대한 j신고대상 중복제거
-            		 System.out.println(B_new.size() + i + "a"); 
+            		System.out.println(B_new.size());  ////////////////
+
             	}
+        	}        	
+        	//id_list[i]에 해당하는 B_new를 이터레이터로 읽어서 String[] BB를 만듬
+        	for(int kk=0; kk<id_list.length; kk++) {
+            	iter = B_new.iterator();
+    			while(iter.hasNext()) {
+        			if(id_list[kk].equals(iter.next())) { // kk유저 == k신고대상
+        				
+        				BB[kk] += 1; // id_list[kk]번째의 신고 횟수를 BB에 저장
+        			}	
+        		}
+        	}
+        	B_new = null;
+        }
+        
+    	for(int j=0; j<id_list.length; j++) {
+            for(int i=0; i<id_list.length; i++) {
+        		if(BB[j] >=k && id_list[j] == B[i]) {
+        			AB[i] += 1;
+        		}
         	}
         }
-        	
-//        	//id_list[i]에 해당하는 B_new를 이터레이터로 읽어서 String[] BB를 만듬
-//        	Iterator<String> iter = B_new.iterator();
-//        	while(iter.hasNext()) {
-//        		for(int kk=0; kk<id_list.length; kk++) {
-//        			if(id_list[kk] == iter.next()) { // kk유저 == k신고대상
-//        				BB[kk] += 1; // id_list[kk]번째의 신고 횟수를 BB에 저장
-//        			}	
-//        		}
-//        	}
-//        	B_new = null;
-//        }
-//
-//
-//    	for(int j=0; j<id_list.length; j++) {
-//            for(int i=0; i<id_list.length; i++) {
-//        		if(BB[j] >=k && id_list[j] == B[i]) {
-//        			AB[i] += 1;
-//        		}
-//        	}
-//        }
-//        
-//    	//////////
-//        for(int i=0; i<id_list.length; i++) {
-//        	System.out.println(AB[i]);
-//        }
         
-
+        for(int i=0; i<id_list.length; i++) {
+        	System.out.println(AB[i]);
+        }
 	}
-
-	
 }
+
