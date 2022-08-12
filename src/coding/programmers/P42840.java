f00a4b50d8d6434f78a9bc77e9aa44b8963d04fd
Mini's note <95896024+Wanghyemin@users.noreply.github.com>
2022-08-12 23:48:34 +0900

    second try

--- src/coding/programmers/P42840.java
+++ src/coding/programmers/P42840.java
@@ -12,6 +12,11 @@ public class P42840 {
 			int[] p2 = new int[size];
 			int[] p3 = new int[size];
 
+			int pa1 = 0;
+			int pa2 = 0;
+			int pa3 = 0;
+						
+			
 			for (int i = 0; i < size; i++) {
 				if ((i + 1) % 5 != 0) {
 					p1[i] = (i + 1) % 5;
@@ -22,12 +27,43 @@ public class P42840 {
 			}
 			for (int i = 0; i < size; i++) {
 				if (i % 2 == 0) {
-					p2[i] = 2;
-				} else {
-
-				}
-
+					p2[i] = 2; 
+				} else if(i%8 == 1){
+					p2[i] = 1;
+				}else if(i%8 == 3) {
+					p2[i] = 3;
+				}else if(i%8 == 5) {
+					p2[i] = 4;
+				}else if(i%8 == 7)
+					p2[i] = 5;
 			}
+			for (int i=0; i < size; i++) {
+				if(i % 10 == 0 || i%10 == 1) {
+					p3[i] = 3;
+				}else if(i % 10 == 2 || i%10 == 3) {
+					p3[i] = 1;
+				}else if(i % 10 == 4 || i%10 == 5) {
+					p3[i] = 2;
+				}else if(i % 10 == 6 || i%10 == 7) {
+					p3[i] = 4;
+				}else if(i % 10 == 8 || i%10 == 9) {
+					p3[i] = 5;
+				}
+			}
+			
+			for(int i=0; i<size; i++) {
+				if(answers[i]==p1[i]) {
+					pa1++;
+				}
+				if(answers[i]==p2[i]) {
+					pa2++;
+				}
+				if(answers[i]==p3[i]) {
+					pa3++;
+				}
+			}
+			
+			
 
 			return p1;
 		}
